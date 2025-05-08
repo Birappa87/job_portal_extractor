0a93e0a38f5c86bd7d75d2badcaeefb8bc5cd77d
Birappa87 <birappa.001@gmail.com>
2025-05-08 19:42:21 +0530

    commit

--- linkedin.py
+++ linkedin.py
@@ -548,7 +548,7 @@ async def load_all_jobs():
             processed_page_count = 0
             
             # LinkedIn jobs search URL
-            URL = "https://www.linkedin.com/jobs/search/?currentJobId=4215342655&f_E=4&f_JT=F&f_SB2=42&f_TPR=r604800&f_WT=1%2C3&geoId=101165590&keywords=&location=United%20Kingdom&origin=JOB_SEARCH_PAGE_JOB_FILTER"
+            URL = "https://www.linkedin.com/jobs/search/?currentJobId=4218290146&f_E=4&f_JT=F&f_SB2=42&f_TPR=r2592000&f_WT=1%2C3&geoId=101165590&keywords=&location=United%20Kingdom&origin=JOB_SEARCH_PAGE_JOB_FILTER"
             
             # Navigate to the page
             await page.goto(URL)
